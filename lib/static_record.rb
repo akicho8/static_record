78e5d6df609439de12023f03382e7af952381bce
akicho8 <akicho8@gmail.com>
2017-09-18 22:21:34 +0900

    DEPRECATION メッセージ追加

--- lib/static_record.rb
+++ lib/static_record.rb
@@ -1,2 +1,4 @@
 require "static_record/version"
 require "static_record/static_record"
+
+ActiveSupport::Deprecation.warn("I do not maintain this library anymore. Please use memory_record gem instead.")
